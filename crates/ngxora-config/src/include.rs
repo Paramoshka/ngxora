268c586fcbd19b93a21145a118f7a79b04ab2435
Paramoshka <parfenov_ivan_42a@mail.ru>
2026-01-07 11:49:58 +0300

    update
    Signed-off-by: Paramoshka <parfenov_ivan_42a@mail.ru>

--- crates/ngxora-config/src/include.rs
+++ crates/ngxora-config/src/include.rs
@@ -1,14 +1,16 @@
-use crate::Ast;
+use crate::{Ast, Block, Node};
+
+const INCLUDE_DIRECTIVE: &str = "include";
 
 #[derive(Debug, Default)]
-pub struct IncludeResolver<'a> {
-    pub includes_files: Vec<IncludeFile<'a>>,
+pub struct IncludeResolver {
+    pub includes_files: Vec<IncludeFile>,
 }
 
 #[derive(Debug)]
-pub struct IncludeFile<'a> {
+pub struct IncludeFile {
     pub path: String,
-    pub text: &'a str,
+    pub text: String,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -17,13 +19,34 @@ pub struct IncludeError {
 }
 
 impl IncludeResolver {
-    pub fn new(&self, ast: Ast) -> IncludeResolver {
+    pub fn new(ast: &Ast) -> Self {
+        let mut includes_files: Vec<IncludeFile> = Vec::new();
+        collect_includes(&ast.items, &mut includes_files);
+
         IncludeResolver {
-            includes_files: Vec::new(),
+            includes_files: includes_files,
         }
     }
 
     pub fn resolve(&self) -> Result<Ast, IncludeError> {
         // TODO replace includes on Nodes
+        todo!()
+    }
+}
+
+fn collect_includes(nodes: &[Node], out: &mut Vec<IncludeFile>) {
+    for node in nodes {
+        match node {
+            Node::Directive(directive) if directive.name == INCLUDE_DIRECTIVE => {
+                for inc_file_path in &directive.args {
+                    out.push(IncludeFile {
+                        path: inc_file_path.clone(),
+                        text: String::new(),
+                    });
+                }
+            }
+            Node::Block(block) => collect_includes(&block.children, out),
+            _ => {}
+        }
     }
 }
